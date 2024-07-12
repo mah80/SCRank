d4d1896d01da24ac8196961b9e3650e8ea63559c
malmukhtar <mohamed.j.almukhtar@gmail.com>
2024-07-12 02:52:50 -0400

    Add excution time helper function Add Calcualte execution time for both git and zip process Display execution time in results.html page

--- tool/helpers/utils.py
+++ tool/helpers/utils.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 def read_csv(file_path):
     with open(file_path, newline='') as csvfile:
@@ -10,3 +11,24 @@ def write_csv(file_path, data):
     with open(file_path, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data)
+
+
+
+def time_difference(start_time: datetime, end_time: datetime) -> str:
+    delta = end_time - start_time
+    days = delta.days
+    seconds = delta.seconds
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    result = []
+    if days > 0:
+        result.append(f"{days} days")
+    if hours > 0:
+        result.append(f"{hours} hours")
+    if minutes > 0:
+        result.append(f"{minutes} mins")
+    if seconds > 0:
+        result.append(f"{seconds} secs")
+
+    return ", ".join(result)
